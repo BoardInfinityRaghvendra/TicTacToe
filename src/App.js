2f9cd5a4e585552b33d3e5e1288df803bdced0ba
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-06-04 22:47:15 +0530

    set up the required 3rd party libraries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react"
+import Icon from "./Components/Icon"
+// react toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// react strap
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Col, Row, Card, CardBody, Container } from 'reactstrap';
+import "./App.css"
 
 
 const App = ()=>{
     return(
-        <h1> Welcome </h1>
+        <Icon choice="circle"/>
     )
 }
 
