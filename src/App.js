e76f2ef1948675c92dd58b891f5d7b2096950a38
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-06-11 22:55:54 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -8,55 +8,55 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Button, Col, Row, Card, CardBody, Container } from 'reactstrap';
 import "./App.css"
 
-tictoktoeArray = new Array(9).fill("")
+const tictoktoeArray = new Array(9).fill("")
 const App = ()=>{
     const [isCross, setIsCross]= useState(true)
-    const [winnerMessage, setWinnerMessage]= useState("")
+    const [winMessage, setWinMessage]= useState("")
 
     const playAgain=() =>{
         setIsCross(true)
-        setWinnerMessage("")
+        setWinMessage("")
         tictoktoeArray.fill("")
     }
 
     const findWinner =({index}) =>{
         if(tictoktoeArray[0] == tictoktoeArray[1] && 
             tictoktoeArray[0] == tictoktoeArray[2]){
-                console.log(tictoktoeArray[0])
+                setWinMessage(tictoktoeArray[0])
             }
         else if(tictoktoeArray[3] == tictoktoeArray[4] && 
                 tictoktoeArray[3] == tictoktoeArray[5]){
-                    console.log(tictoktoeArray[3])
+                    setWinMessage(tictoktoeArray[3])
             }
         else if(tictoktoeArray[6] == tictoktoeArray[7] && 
                 tictoktoeArray[6] == tictoktoeArray[8]){
-                console.log(tictoktoeArray[6])
+                    setWinMessage(tictoktoeArray[6])
             }
         else if(tictoktoeArray[0] == tictoktoeArray[3] && 
             tictoktoeArray[0] == tictoktoeArray[6]){
-            console.log(tictoktoeArray[0])
+                setWinMessage(tictoktoeArray[0])
         }
         else if(tictoktoeArray[1] == tictoktoeArray[4] && 
             tictoktoeArray[1] == tictoktoeArray[5]){
-            console.log(tictoktoeArray[1])
+                setWinMessage(tictoktoeArray[1])
         }
         else if(tictoktoeArray[2] == tictoktoeArray[5] && 
             tictoktoeArray[2] == tictoktoeArray[8]){
-            console.log(tictoktoeArray[2])
+                setWinMessage(tictoktoeArray[2])
         }
         else if(tictoktoeArray[0] == tictoktoeArray[4] && 
             tictoktoeArray[0] == tictoktoeArray[8]){
-            console.log(tictoktoeArray[0])
+                setWinMessage(tictoktoeArray[0])
         }
         else if(tictoktoeArray[2] == tictoktoeArray[4] && 
             tictoktoeArray[2] == tictoktoeArray[6]){
-            console.log(tictoktoeArray[2])
+                setWinMessage(tictoktoeArray[2])
         }
     }
 
-    const changeIcon =() =>{
-          if(winnerMessage){
-            return toast.success(winnerMessage, {
+    const changeIcon =({index}) =>{
+          if(winMessage){
+            return toast.success("The game has already ended please reload", {
                 position: toast.POSITION.BOTTOM_RIGHT
               });
           } 
@@ -64,12 +64,43 @@ const App = ()=>{
               return  isCross? "cross" : "circle"
           }
           else{
-            return toast.success("select a diffferent box", {
+            return toast.success("select a different box", {
                 position: toast.POSITION.BOTTOM_RIGHT
               }); 
           }
         
     }
+
+
+    return(
+        <Container className="p-5">
+
+           <Row>
+                <Col md={6} className="offset-md-3">
+                    
+                      <div className="grid">
+                      {tictoktoeArray.map((value,index)=>(
+                        <Card onClick={}>
+                           <CardBody className="box">
+                              <Icon choice={value}/>   
+                           </CardBody>
+                        </Card>
+                      ))}
+                      </div> 
+                       
+                    
+                    
+                     
+                </Col>
+
+           </Row>
+
+
+        </Container>
+
+
+        
+    )
     
 }
 
